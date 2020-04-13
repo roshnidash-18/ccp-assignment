b6f6da57a7df922c27311cd96c7a424c3986a052
roshnidash-18 <63587904+roshnidash-18@users.noreply.github.com>
2020-04-13 16:45:34 +0530

    Update average.c

--- average.c
+++ average.c
@@ -3,9 +3,9 @@ int main()
 {
   float arr[50],avg,sum=0;
   int m,i;
-  printf("input how many  numbers to be averaged\t");
+  printf("Input how many  numbers to be averaged\t");
   scanf("%d",&m);
-  printf("enter the values \t");
+  printf("Enter the values \t");
   for(i=0;i<m;i++){
     scanf("%f",&arr[i]);
 }
@@ -14,5 +14,5 @@ for(i=0;i<m;i++)
   sum+=arr[i];
 }
 avg=sum/m;
-printf("\n the average of %d numbers is= %f\n",m,avg);
+printf("\n The average of %d numbers is= %f\n",m,avg);
 }
